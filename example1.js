7065767b8b842df006b728b2586bb7227814dace
monogordo <ramtin.radfar@gmail.com>
2017-01-18 09:34:33 -0800

    Add first React component

--- example1.js
+++ example1.js
@@ -1,1 +1,14 @@
-alert('hey');
+class AppComponent extends React.Component {
+	render() {
+		return React.createElement(
+			"section",
+			{className: "site-wrap"},
+			React.createElement("h1", null, "Header"),
+			React.createElement("p", null, "blah"));
+	}
+}
+
+ReactDOM.render(
+	React.createElement(AppComponent, null),
+	document.getElementById("application")
+);
